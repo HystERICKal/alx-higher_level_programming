a8a5fbc8ae7ac5e78b9d69a20237e4a448e3673f
HystERICKal <nyoroerick12@gmail.com>
2023-10-31 09:45:16 -0700

    Task 1 - Fix Spacing issue

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    ending_digit = number % -10  #handle negatives
+    ending_digit = number % -10  # handle negatives
 elif number >= 0:
-    ending_digit = number % 10  #handle positives
+    ending_digit = number % 10  # handle positives
 if ending_digit > 5:
     print(f"Last digit of {number} is {ending_digit} and is greater than 5")
 elif ending_digit == 0:
     print(f"Last digit of {number} is {ending_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {ending_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is\
+ {ending_digit} and is less than 6 and not 0")
