6a9869045db0aa19e9f4c93f73e32cf6176a078d
HystERICKal <nyoroerick12@gmail.com>
2023-11-06 11:47:02 -0800

    Advanced task 0

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info - prints some basic info about pylists
+ * @p: the infamous list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int x = 0; /*counter*/
+
+	/*get size of the PyObject like this...*/
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+
+	/*Allocated elements*/
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+
+	while (x < Py_SIZE(p))
+	{
+		/*Name of the type*/
+		printf("Element %d: %s\n", x, Py_TYPE(PyList_GetItem(p, x))->tp_name);
+		x++;
+	}
+
+}
