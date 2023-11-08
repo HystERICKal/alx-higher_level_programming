1f9132704b6f4a35b5c749f33c06d05cf461ed6f
HystERICKal <nyoroerick12@gmail.com>
2023-11-08 10:29:11 -0800

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    the_roman_reference = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    outer = []
+    for s in roman_string:
+        outer.append(the_roman_reference[s])
+    final_int = 0
+    i = 0
+    while i < len(outer):
+        final_int = final_int + outer[i]
+        if outer[i - 1] < outer[i] and i is not 0:
+            final_int = final_int - (outer[i - 1] + outer[i - 1])
+        i = i + 1
+    return final_int
