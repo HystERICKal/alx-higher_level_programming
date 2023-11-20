7026353f1816d86002a83d9f3a6cff87d02afbea
HystERICKal <nyoroerick12@gmail.com>
2023-11-20 08:02:27 -0800

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div_value = a / b
+    except ZeroDivisionError:
+        div_value = None
+    finally:
+        print("Inside result: {}".format(div_value))
+    return div_value
