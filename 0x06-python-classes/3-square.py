0f36fca792059a2305cd68c2b02c322e08498385
HystERICKal <nyoroerick12@gmail.com>
2023-11-21 09:19:50 -0800

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This class defines a Square"""
+
+
+class Square:
+    """This class defines a Square"""
+
+    def __init__(self, size):
+        """The initialization of a brand new square.
+        Args:
+            size (none type): the new square is of this size.
+        """
+        if (isinstance(size, int)):
+            self.__size = size
+            if size < 0:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """This determine's the square's area."""
+        return (self.__size * self.__size)
