d0b0faf05892a7d025c9aaf6746b6124d951d902
HystERICKal <nyoroerick12@gmail.com>
2023-11-21 11:02:47 -0800

    Bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python bytecode"""
+import math
+
+
+class MagicClass:
+    """The MagicClass Class"""
+    def __init__(self, radius=0):
+        """the constructor of the magic class"""
+        self._MagicClass__radius = 0
+        if type(radius) is not int:
+            raise TypeError("radius must be a number")
+        if type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """The area of the magic class"""
+        return self._MagicClass__radius * self._MagicClass__radius * math.pi
+
+    def circumference(self):
+        """The circumference of the magic class"""
+        return 2 * math.pi * self._MagicClass__radius
