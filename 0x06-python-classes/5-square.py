72d0b2d2e4264b422ae82f552dfcbdd02dac08ca
HystERICKal <nyoroerick12@gmail.com>
2023-11-21 10:06:14 -0800

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This class defines a Square"""
+
+
+class Square:
+    """This class defines a Square"""
+
+    def __init__(self, size=0):
+        """The initialization of a brand new square.
+        Args:
+            size (none type): the new square is of this size.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """The Square's new size innit"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int)):
+            self.__size = value
+            if value < 0:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """This determine's the square's area."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """This one prints a square with #"""
+        x = 0
+        y = 0
+
+        while x < self.__size:
+            while y < self.__size:
+                print("#", end="")
+                y += 1
+            print("")
+            x += 1
+        if self.__size == 0:
+            print("")
