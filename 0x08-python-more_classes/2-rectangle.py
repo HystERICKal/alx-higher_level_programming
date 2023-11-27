3a51ad4f36ccf0cd31559130222f0e9be85d8eb0
HystERICKal <nyoroerick12@gmail.com>
2023-11-27 10:04:08 -0800

    Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This is a Rectangle class
+"""
+
+
+class Rectangle:
+    """The Rectangle class with added functionality"""
+
+    def __init__(self, width=0, height=0):
+        """Constructor of Rectangle class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Private instance of attribute width retrieved here"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Private instance of attribute width is set here"""
+        if isinstance(value, int):
+            self.__width = value
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """Private instance of attribute height retrieved here"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Private instance of attribute height is set here"""
+        if isinstance(value, int):
+            self.__height = value
+            if value < 0:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """Rectangle Area is returned"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Rectangle Perimeter is returned"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height + self.__width + self.__height)
