93ef6055a6651dbfc44bcf6f5b52248b6979d8c9
HystERICKal <nyoroerick12@gmail.com>
2023-12-05 09:24:35 -0800

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This function returns JSON rep of an object """
+import json
+
+
+def to_json_string(my_obj):
+    """ This function returns JSON rep of an object """
+    return json.dumps(my_obj)
