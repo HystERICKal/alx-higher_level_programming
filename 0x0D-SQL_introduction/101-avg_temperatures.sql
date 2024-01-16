351582e8c7b2f8f7c8c329ba4734ac3029524c08
HystERICKal <nyoroerick12@gmail.com>
2024-01-16 09:47:58 -0800

    task 101

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Script does temperature display
+SELECT city, avg(value) AS avg_temp FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,4 @@
 --Cheating is horrible update Bob's score to 10
-UPDATE second_table SET score = 10 WHERE name = "Bob";
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
