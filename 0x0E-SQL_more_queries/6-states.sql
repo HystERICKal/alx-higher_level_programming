d2e9ff4d78e0e62aa7740609becb61bd70a2893f
HystERICKal <nyoroerick12@gmail.com>
2024-01-17 07:53:42 -0800

    Task 3,4,5,6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creating a database and table inside it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE 'hbtn_0d_usa';
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY(id),
+	name VARCHAR(256) NOT NULL
+);
