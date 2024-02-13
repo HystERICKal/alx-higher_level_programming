2244ef457e87e31817fc14218f45cc67334765e4
HystERICKal <nyoroerick12@gmail.com>
2024-02-13 08:29:42 -0800

    Task 1-10 Fix errors

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
     }
-  }
   
-  print () {
-    let x = 0;
-    while (x < this.height) {
-      console.log('X'.repeat(this.width));
-      x++;
+    print () {
+      let x = 0;
+      while (x < this.height) {
+        console.log('X'.repeat(this.width));
+        x++;
+      }
+    }
+  
+  
+   rotate () {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  
+    double () {
+      this.width = this.width * 2;
+      this.height = this.height * 2;
     }
   }
-}
-
- rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,11 @@
 const inh_square = require('./5-square.js');
 
 module.exports = class Square extends inh_square {
-  charPrint (l) {
-    if (l === undefined) { this.print(); } else {
-        let x = 0;
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      let x = 0;
       while (x < this.height) {
-        console.log(l.repeat(this.width));
+        console.log(c.repeat(this.width));
         x++;
       }
     }
