3ff9ff0b700dc81d76d2fcf14712a6cd6e208ec7
HystERICKal <nyoroerick12@gmail.com>
2024-02-13 08:44:15 -0800

    Advanced Tasks fix err

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const the_dict = require('./101-data.js').dict;
-const reslt = {
-  1: Object.keys(the_dict).filter(key => the_dict[key] === 1),
-  2: Object.keys(the_dict).filter(key => the_dict[key] === 2),
-  3: Object.keys(the_dict).filter(key => the_dict[key] === 3)
+const dict = require('./101-data.js').dict;
+const output = {
+  1: Object.keys(dict).filter(key => dict[key] === 1),
+  2: Object.keys(dict).filter(key => dict[key] === 2),
+  3: Object.keys(dict).filter(key => dict[key] === 3)
 };
-console.log(reslt);
+console.log(output);
