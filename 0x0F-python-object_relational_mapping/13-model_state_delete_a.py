5c7da9c6af22da190a35aeca8491492bcb8fd97f
HystERICKal <nyoroerick12@gmail.com>
2024-02-18 10:13:51 -0800

    Task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+prints first State object from the database hbtn_0e_6_usa
+'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    reslt = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]))
+    Base.metadata.create_all(reslt)
+    the_sesh = sessionmaker(bind=reslt)
+    sesh = the_sesh()
+    for x in sesh.query(State).filter(State.name.like('%a%')).all():
+        sesh.delete(x)
+    sesh.commit()
+    sesh.close()
