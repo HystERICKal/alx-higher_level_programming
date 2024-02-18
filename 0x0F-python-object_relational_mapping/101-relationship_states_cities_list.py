3cd200813dc552935605dad058e050e8995925a6
HystERICKal <nyoroerick12@gmail.com>
2024-02-18 10:55:45 -0800

    Task 101

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Task 101"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+
+if __name__ == "__main__":
+    reslt = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                          .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(reslt)
+    the_sesh = sessionmaker(bind=reslt)
+    sesh = the_sesh()
+    for x in sesh.query(State).order_by(State.id):
+        print(x.id, x.name, sep=": ")
+        for y in x.cities:
+            print("    ", end="")
+            print(y.id, y.name, sep=": ")
