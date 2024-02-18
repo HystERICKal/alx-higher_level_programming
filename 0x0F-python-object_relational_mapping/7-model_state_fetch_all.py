8d0d6230318adddd5c4439a5c64418dfd0948164
HystERICKal <nyoroerick12@gmail.com>
2024-02-18 09:38:27 -0800

    Task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''lists all State objects from the database hbtn_0e_6_usa'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    reslt = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]))
+    Base.metadata.create_all(reslt)
+    the_sesh = sessionmaker(bind=reslt)
+    sesh = the_sesh()
+    outcome = sesh.query(State).order_by(State.id)
+    for state in outcome:
+        print('{}: {}'.format(state.id, state.name))
+    sesh.close()
