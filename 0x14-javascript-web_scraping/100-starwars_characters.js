537b913a6cd596fa564959f8ba0f8ac7c0b72444
HystERICKal <nyoroerick12@gmail.com>
2024-03-26 09:25:59 -0700

    Task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// movie characters of star wars movie printed
+
+const request = require('request');
+
+const pass = process.argv.slice(2)[0];
+const theLink = `https://swapi-api.hbtn.io/api/films/${pass}`;
+
+request(theLink, (error, res, body) => {
+  if (error) console.log(error);
+  const cast = JSON.parse(body).cast;
+  for (const i in cast) {
+    request(cast[i], (error, res, body) => {
+      if (error) console.log(error);
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
