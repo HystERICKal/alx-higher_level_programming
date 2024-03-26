22508bf60beb16b9a837fa9ebe581161c614bc67
HystERICKal <nyoroerick12@gmail.com>
2024-03-26 09:06:17 -0700

    Task 3 err3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Printing star wars movie title
 
-const request = require("request");
+const request = require('request');
 
 const digi = process.argv.slice(2)[0];
-const the_link = `https://swapi-api.hbtn.io/api/films/${digi}`;
+const theLink = `https://swapi-api.hbtn.io/api/films/${digi}`;
 
-request(the_link, (err, res, body) => {
+request(theLink, (err, res, body) => {
   if (err) console.log(err);
   const result = JSON.parse(body).title;
   console.log(result);
