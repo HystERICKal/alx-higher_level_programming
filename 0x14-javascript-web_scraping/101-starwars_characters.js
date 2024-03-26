ec1e6a2eb0572597edd4b5c72a5ca0751f464abd
HystERICKal <nyoroerick12@gmail.com>
2024-03-26 09:46:35 -0700

    Task 8 err2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
+// star wars movie characters printed with this script
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const pass = process.argv[2];
+const theLink = `https://swapi-api.alx-tools.com/api/films/${pass}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
+request.get(theLink, (err, response, body) => {
+  if (err) {
+    console.log(err);
   } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
+    const bodyStuff = JSON.parse(body);
+    const cast = bodyStuff.characters;
+    for (const i of cast) {
+      request.get(i, (err, response, body) => {
+        if (err) {
+          console.log(err);
         } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+          const castName = JSON.parse(body);
+          console.log(castName.name);
         }
       });
     }
